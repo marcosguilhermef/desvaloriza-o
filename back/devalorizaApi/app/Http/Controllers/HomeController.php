c751ddc005bc6867eec26b01a762b3317600600a
marcosguilhermefh@gmail.com <marcosguilhermefh@gmail.com>
2021-05-16 23:06:40 -0300

    mudança das tags facebook

--- back/devalorizaApi/app/Http/Controllers/HomeController.php
+++ back/devalorizaApi/app/Http/Controllers/HomeController.php
@@ -10,15 +10,15 @@ class HomeController extends Controller
     public function __invoke()
     {
         $meta = [
-        "tag" =>'<title>Desvalorização</title>
-    <meta name="description" content="Desvalorização é uma ferramenta criada para mostrar a perda do poder de compra do real nos últimos anos e como isso afeta diretamente na vida dos brasileiros." />
-    <meta name="facebook-domain-verification" content="dz0rtvkf5s20ww9d9g1m8j960kp7bv" />
-    <meta property="og:url"            content="http://desvalorizacao.com/" />
-    <meta property="og:type"           content="site" />
-    <meta property="og:title"          content="Quando o seu dinheiro se desvalorizou nos últimos anos? " />
-    <meta property="og:description"    content="Essa ferramenta tem o objetico de demonstrar o tanto que o seu dinheiro se desvalorizou nos últimos anos." />
-    <meta property="og:image"          content="https://desvalorizacao.com/image/android-chrome-512x512.png" />'
-        ];
+            "tag" =>'<title>Desvalorização</title>
+            <meta name="description" content="Desvalorização é uma ferramenta que te ajuda a entender os efeitos da inflação sobre o poder de compra da população brasileira. Usando os Índices IPCA (Índice de preços para consumidor amplo) e IPCM (Índice de preços ao consumidor - Merdado), monta-se um panorama da desvalorização do real no decorrer dos anos. Dessa forma, é mostrado valores de alguns produtos em determinadas épocas e seus respectivos valores junto aos dados dá inflação. Portando, é esperado que o usuário entenda como a inflação afeta-o diretamente." />
+            <meta name="facebook-domain-verification" content="dz0rtvkf5s20ww9d9g1m8j960kp7bv" />
+            <meta property="og:url"            content="http://desvalorizacao.com/" />
+            <meta property="og:type"           content="site" />
+            <meta property="og:title"          content="Será que o seu dinheiro ainda vale para alguma coisa? Descubra!" />
+            <meta property="og:description"    content="Desvalorização é uma ferramenta que te ajuda a entender os efeitos da inflação sobre o poder de compra da população brasileira. Usando os Índices IPCA (Índice de preços para consumidor amplo) e IPCM (Índice de preços ao consumidor - Merdado), monta-se um panorama da desvalorização do real no decorrer dos anos. Dessa forma, é mostrado valores de alguns produtos em determinadas épocas e seus respectivos valores junto aos dados dá inflação. Portando, é esperado que o usuário entenda como a inflação afeta-o diretamente." />
+            <meta property="og:image"          content="https://desvalorizacao.com/image/android-chrome-512x512.png" />'
+            ];
         return Inertia::render('Home',$meta);
     }
 }
